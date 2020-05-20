37660fb57057e678037e941609c35582bfc10d9d
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-05-20 16:11:43 -0400

    Updates

---
+++ precache-manifest.986ac07f70e3ad90ef86871717893f90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ef6f18fb43b00d6966ce2495fdeaf8",
+    "url": "/react-color-app/index.html"
+  },
+  {
+    "revision": "d2235e153bdcc680b618",
+    "url": "/react-color-app/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "1d2355bbc765e2d69a67",
+    "url": "/react-color-app/static/css/main.8c2ed54f.chunk.css"
+  },
+  {
+    "revision": "d2235e153bdcc680b618",
+    "url": "/react-color-app/static/js/2.dcd07a93.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react-color-app/static/js/2.dcd07a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2355bbc765e2d69a67",
+    "url": "/react-color-app/static/js/main.af8d9418.chunk.js"
+  },
+  {
+    "revision": "11cb89f36b5a65029ae0",
+    "url": "/react-color-app/static/js/runtime-main.ba8a3ce9.js"
+  },
+  {
+    "revision": "38843277e74366c2ce16d48827dad071",
+    "url": "/react-color-app/static/media/bg.38843277.svg"
+  }
+]);
